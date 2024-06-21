6bd240db9afacf6ca21c67449bc12ce383faa0a9
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-22 08:24:46 +0900

    station15 clear

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,30 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+
+  // 3秒待機するPromiseを作成する関数
+  function wait(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+  // 3秒後にフルネームのリストを返すPromise
+  const result = await wait(3000).then(() => {
+    return userList.map(buildFullName);
+  });
+
+  return result;
 }
 
+// フルネームを作成する関数
+function buildFullName(data) {
+  const full_name = `${data.family_name} ${data.first_name}`;
+  return { ...data, full_name };
+}
+
+// test関数をPromiseを返す形式に変更
 function test() {
-  return
+  return new Promise(resolve => {
+    resolve(getData());
+  });
 }
 
